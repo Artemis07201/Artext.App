99d6a37fe75099cd4d1e3a8a44cff89e36c19302
Izolabella R. Geddis <iza.yy10@gmail.com>
2022-08-28 00:55:01 -0400

    ☕ mOCHA

--- ArtextFileDisplay.xaml.cs
+++ ArtextFileDisplay.xaml.cs
@@ -1,9 +1,24 @@
-namespace Artext;
-
-public partial class ArtextFileDisplay : ContentPage
+namespace Artext
 {
-	public ArtextFileDisplay()
-	{
-		InitializeComponent();
-	}
+    public partial class ArtextFileDisplay : ContentPage
+    {
+        public ArtextFileDisplay()
+        {
+            InitializeComponent();
+        }
+
+        private void ContentPage_SizeChanged(object Sender, EventArgs E)
+        {
+            double TX = MainGrid.Width;
+            double TY = MainGrid.Height;
+
+            EDIT.WidthRequest = TX;
+            EDIT.HeightRequest = TY;
+        }
+
+        private void ContentPage_Appearing(object Sender, EventArgs E)
+        {
+            this.ContentPage_SizeChanged(Sender, E);
+        }
+    }
 }
